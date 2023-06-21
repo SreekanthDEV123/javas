127f190dd628fa002bf897e952831e90e20ee0d9
SreekanthDev123 <sreekanthraj0@gmail.com>
2023-06-21 17:21:11 +0530

    first

--- ASyn-Sync-TIMEout_TIMEINterval/object1.js
+++ ASyn-Sync-TIMEout_TIMEINterval/object1.js
@@ -1,7 +1,18 @@
-console.log("super");
-for(let  i=0; i<10; i++)
-{
-    console.log("handsome");
 
-}
+console.log("super");
+//for(let  i=0; i<10; i++)
+//{
+    setTimeout(()=>
+    {
+        test();
+    }
+
+    ,5000)
+    test();
+
+//}
 console.log("good");
+ function test()
+ {
+    console.log("sreekanth")
+ }
